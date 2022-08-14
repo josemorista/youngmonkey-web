00334561ab8c6ac7f4a87726b2f65f951c30e067
josemorista <josemorista@id.uff.br>
2022-08-14 10:15:22 -0300

    feat: added formData submit

--- src/compositors/formStep.ts
+++ src/compositors/formStep.ts
@@ -1,4 +1,5 @@
 import { ref, computed } from 'vue';
+import { useFormData } from './formData';
 
 const currentStep = ref(1);
 const highestStep = ref(1);
@@ -12,9 +13,36 @@ const onNextClick = () => {
   currentStep.value++;
 };
 
-const onStepSubmit = (step: number) => {
+const onStepSubmit = async (step: number) => {
   if (step === highestStep.value) {
     highestStep.value++;
+  }
+  if (step === 7) {
+    const formData = useFormData();
+    const form = new FormData();
+    form.append('entry.640831328', formData.name);
+    form.append('entry.1481678986', formData.enterprise);
+    for (const option of formData.contractOptions) {
+      form.append('entry.1958347428', option);
+    }
+    form.append('entry.1056916052', formData.referenceImg);
+    form.append('entry.462087437', formData.requestedDuration);
+    form.append('entry.603675729', formData.phone);
+    form.append('entry.391128899', formData.other);
+    try {
+      await fetch(
+        'https://docs.google.com/forms/u/0/d/e/1FAIpQLSdK6JSFNZjt1yXwmVKksxMS7IsvbeStjgRHa054_hjIQnlNYg/formResponse',
+        {
+          method: 'POST',
+          body: form,
+          headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+          },
+        }
+      );
+    } catch (error) {
+      console.error(error);
+    }
   }
   onNextClick();
 };

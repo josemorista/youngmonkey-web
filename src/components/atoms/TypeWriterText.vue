99c07cc56888d03cfb3280430f91afef7ed42854
josemorista <josemorista@id.uff.br>
2022-09-15 23:29:49 -0300

    feat: typewriter effect to hero

--- src/components/atoms/TypeWriterText.vue
+++ src/components/atoms/TypeWriterText.vue
@@ -3,14 +3,15 @@
 </template>
 
 <script setup lang="ts">
-import { computed, onMounted, ref, defineProps } from 'vue';
+import { computed, onMounted, ref, defineProps, defineExpose } from 'vue';
 
 interface TypeWriterTextProps {
 	text: string;
-	delay: number;
-	initialDelay?: number;
+	reverseDelay: number;
+	startDelay: number;
 	changeTime: number;
 	customClass?: string;
+	iterationCount?: number;
 }
 
 const props = defineProps<TypeWriterTextProps>();
@@ -18,10 +19,18 @@ const currentIndex = ref(0);
 const reverse = ref(false);
 const text = computed(() => props.text.slice(0, currentIndex.value));
 const sleep = (amount: number) => new Promise((resolve) => setTimeout(resolve, amount));
+let timeout: number;
 
-const toggleReverse = async () => {
-	await sleep(props.delay);
+const toggleReverse = async (waitTime: number) => {
+	await sleep(waitTime);
 	reverse.value = !reverse.value;
+};
+
+const reset = () => {
+	timeout && clearTimeout(timeout);
+	currentIndex.value = 0;
+	reverse.value = false;
+	timeout = setTimeout(updateCurrentText, props.startDelay);
 };
 
 const updateCurrentText = async () => {
@@ -30,17 +39,21 @@ const updateCurrentText = async () => {
 		nextIndex = currentIndex.value - 1;
 		if (nextIndex < 0) {
 			nextIndex = 1;
-			await toggleReverse();
+			return;
 		}
 	} else if (nextIndex > props.text.length) {
 		nextIndex = props.text.length - 1;
-		await toggleReverse();
+		await toggleReverse(props.reverseDelay);
 	}
 	currentIndex.value = nextIndex;
-	setTimeout(updateCurrentText, props.changeTime);
+	timeout = setTimeout(updateCurrentText, props.changeTime);
 };
 
 onMounted(() => {
-	setTimeout(updateCurrentText, props.initialDelay || props.delay);
+	timeout = setTimeout(updateCurrentText, props.startDelay);
+});
+
+defineExpose<{ reset: () => void }>({
+	reset,
 });
 </script>
--- src/components/templates/Hero.vue
+++ src/components/templates/Hero.vue
@@ -3,10 +3,39 @@
 		<img src="../../assets/img/hero.svg" alt="hero" />
 		<div class="hero-texts">
 			<h1>
-				<TypeWriterText :text="$tc('global.video', 2)" :delay="20000" :change-time="500" />
-				<TypeWriterText custom-class="highlight" :text="$tc('global.creative', 2)" :delay="5000" :change-time="300" />
-				<TypeWriterText :text="$t('hero.for_your')" :delay="10000" :change-time="300" />
-				<TypeWriterText custom-class="highlight" :text="$tc('global.idea', 2)" :delay="15000" :change-time="300" />
+				<TypeWriterText
+					ref="t1Ref"
+					:text="texts[0]"
+					:reverse-delay="t1RevDelay"
+					:start-delay="0"
+					:change-time="changeTime"
+				/>
+
+				<TypeWriterText
+					ref="t2Ref"
+					custom-class="highlight"
+					:reverse-delay="t2RevDelay"
+					:text="texts[1]"
+					:start-delay="t2initDelay"
+					:change-time="changeTime"
+				/>
+
+				<TypeWriterText
+					ref="t3Ref"
+					:reverse-delay="t3RevDelay"
+					:text="texts[2]"
+					:start-delay="t3initDelay"
+					:change-time="changeTime"
+				/>
+
+				<TypeWriterText
+					ref="t4Ref"
+					:reverse-delay="0"
+					custom-class="highlight"
+					:text="texts[3]"
+					:start-delay="t4initDelay"
+					:change-time="changeTime"
+				/>
 			</h1>
 			<p class="do-different">
 				{{ $t('hero.do_different') }}
@@ -20,8 +49,36 @@
 </template>
 
 <script setup lang="ts">
+import { computed, onMounted, ref } from 'vue';
+import { useI18n } from 'vue-i18n';
 import Button from '../atoms/Button.vue';
 import TypeWriterText from '../atoms/TypeWriterText.vue';
+const { t } = useI18n();
+const changeTime = 220;
+const texts = computed(() => [t('global.video', 2), t('global.creative', 2), t('hero.for_your'), t('global.idea', 2)]);
+
+const t2initDelay = computed(() => texts.value[0].length * changeTime);
+const t3initDelay = computed(() => texts.value[1].length * changeTime + t2initDelay.value);
+const t4initDelay = computed(() => texts.value[2].length * changeTime + t3initDelay.value);
+
+const textsSize = computed(() => texts.value.reduce((acum, el) => acum + el.length, 0));
+const t1RevDelay = computed(() => (textsSize.value - texts.value[0].length) * 2 * changeTime);
+const t2RevDelay = computed(() => (texts.value[2].length + texts.value[3].length) * 2 * changeTime);
+const t3RevDelay = computed(() => texts.value[3].length * 2 * changeTime);
+
+const t1Ref = ref();
+const t2Ref = ref();
+const t3Ref = ref();
+const t4Ref = ref();
+
+onMounted(() => {
+	setInterval(() => {
+		t1Ref.value?.reset();
+		t2Ref.value?.reset();
+		t3Ref.value?.reset();
+		t4Ref.value?.reset();
+	}, textsSize.value * changeTime * 2);
+});
 </script>
 const
 <style lang="scss" scoped>
@@ -52,6 +109,7 @@ const
 		font-size: 6.4rem;
 		font-family: 'Bebas Neue';
 		span {
+			margin-right: 1rem;
 			font-family: 'Bebas Neue';
 			font-size: inherit;
 			&.highlight {
